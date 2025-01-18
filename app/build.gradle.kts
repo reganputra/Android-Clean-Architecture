a0eb2066bf1b0a0341598575115c1775e8176a84
reganputra <ramadhanregan334@gmail.com>
2025-01-18 12:57:28 +0700

    chores: add network library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,9 @@ dependencies {
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
     androidTestImplementation(libs.room.testing)
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
